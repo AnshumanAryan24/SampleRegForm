11969032223ce3c8a7ec2ad380b419c2c8a620e0
AnshumanAryan24 <115440592+AnshumanAryan24@users.noreply.github.com>
2023-05-21 13:56:28 +0530

    Add files via upload
    Adding files for HTML, CSS and JS

---
+++ app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const class1Elements = document.querySelectorAll('.class1');
+class1Elements.forEach((element) => observer.observe(element));
